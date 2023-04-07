1429b78e311a0305b77459946a130e84a6552947
Nahuel <nahuelmath@gmail.com>
2023-04-07 13:15:46 -0400

    refactor: player movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,15 +7,22 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction = Input.get_axis("ui_left", "ui_right")
+	var x_direction = Input.get_axis("left", "right")
 	if x_direction:
 		velocity.x = x_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	var y_direction = Input.get_axis("ui_up", "ui_down")
+	var y_direction = Input.get_axis("up", "down")
 	if y_direction:
 		velocity.y = y_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("eventHorizon"):
+		event_horizon()
+		
+func event_horizon():
+	pass
