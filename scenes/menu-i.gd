069b07d9982f3484025ca3f10151319f68abd28d
cata <cat2020ap@gmail.com>
2023-04-08 18:34:24 -0400

    menu de inicio y de pausa agregados

---
+++ scenes/menu-i.gd
@@ -0,0 +1,16 @@
+extends MarginContainer
+
+@onready var play = %Inicio
+@onready var exit = %Salir
+
+@export var main_scene: PackedScene
+
+func _ready():
+	play.pressed.connect(_on_play_pressed)
+	exit.pressed.connect(_on_exit_pressed)
+
+func _on_play_pressed():
+	#get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_packed(main_scene)
+func _on_exit_pressed():
+	get_tree().quit()
