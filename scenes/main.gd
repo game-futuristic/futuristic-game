220a022ea78101ac7ceb2b49eb71a07b36b1d38a
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-04-23 17:17:12 -0400

    primer borrador fix Event Horizon

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,8 +4,10 @@ extends Node2D
 @export var EnemyScene: PackedScene
 @onready var line = $Line2D
 @onready var closeLine = $closeLine
+@export var StartOfZone: PackedScene
 
 var player
+var startZone
 var enemy1
 var enemy2
 var enemy3
@@ -15,6 +17,7 @@ var endPoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	startZone = StartOfZone.instantiate()
 	player = PlayerScene.instantiate()
 	add_child(player)
 	var x_pos = 50
@@ -25,29 +28,34 @@ func _ready():
 		i += 2
 		add_child(e)
 		e.add_to_group("enemies")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("eventHorizon"):
 		line.add_point(player.get_global_position())
-		draw_line(initialPoint, player.get_global_position(), Color(255, 255, 255))
-#		queue_redraw()
 	if Input.is_action_just_released("eventHorizon"):
-		draw_line(initialPoint, player.get_global_position(), Color(255, 255, 255))
-		var all_enemies = get_tree().get_nodes_in_group("enemies")
-		for e in all_enemies:
-			if Geometry2D.is_point_in_polygon(e.global_position, line.points):
-				e.queue_free()
-		line.points = []
+		if (player.onZone == -3):
+			var all_enemies = get_tree().get_nodes_in_group("enemies")
+			for e in all_enemies:
+				if Geometry2D.is_point_in_polygon(e.global_position, line.points):
+					e.queue_free()
+			startZone.queue_free()
+			print("punto final: ", line.points[-1])
+			line.points = []
+		else:
+			startZone.queue_free()
+			line.points = []
 
 func _physics_process(delta):
 	var player_position = player.get_global_position()
-#	enemy.shoot(player_position)
 
-
-# Agregar una linea punteada siempre desde la posicion del jugador al inicio cosa de que demarque la zona que se creara
-# y donde se aplciara el danho
 func _input(event):
 	if Input.is_action_just_pressed("eventHorizon"):
+		startZone = StartOfZone.instantiate()
 		initialPoint = player.get_global_position()
-		print(initialPoint)
+		startZone.global_position = initialPoint
+		add_child(startZone)
+		print("punto inicial: ", initialPoint)
+#	if Input.is_action_just_released("eventHorizon"):
+#			startZone.queue_free()
--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
+var onZone = 0
+
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
@@ -18,12 +20,6 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
-		
 
-#func _unhandled_input(event):
-#	if event.is_action_released("eventHorizon"):
-#		event_horizon(event)
-#		print("se apreto")
-#
-#func event_horizon(event):
-#	pass
+func startZone(v):
+	onZone += v
